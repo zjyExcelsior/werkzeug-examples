0fe7fc8a0bc0218646f7525722e526f2acc07fcf
Zhu Jiongyao <zjywade@qq.com>
2016-05-06 23:09:51 +0800

    add some middleware

--- testWerkzeug.py
+++ testWerkzeug.py
@@ -1,9 +1,11 @@
 #coding: utf-8
 from werkzeug.utils import escape
 from werkzeug.wrappers import Request, Response
+from werkzeug.debug import DebuggedApplication
 
 @Request.application
 def hello_world(request):
+    1/0
     result = ['<title>Greeeter</title>']
     if request.method == 'POST':
         result.append('<h1>Hello %s!</h1>' % escape(request.form['name']))
@@ -17,4 +19,5 @@ def hello_world(request):
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
+    hello_world = DebuggedApplication(hello_world, evalex=True)
     run_simple('localhost', 5000, hello_world)
